5fabc61eacdd48fc7dd8605084bacfc54ce5fe05
mofarih-dev <78072650+mofarih-dev@users.noreply.github.com>
2022-10-29 11:37:01 +0300

    upload project to github

---
+++ src/app.js
@@ -0,0 +1,13 @@
+let value = document.getElementById("value");
+let total = document.getElementById("total");
+let result = document.getElementById("result");
+let calcBtn = document.getElementById("calc-btn");
+
+calcBtn.addEventListener("click", () => {
+  if ((value.value !== "") & (total.value !== "")) {
+    let finalResult = (value.value / total.value) * 100;
+    result.innerHTML = `${String(finalResult).length > 3 ? String(finalResult).slice(0,5) : finalResult}%`;
+  } else {
+    alert("من فضلك املأ كل الحقول ");
+  }
+});
